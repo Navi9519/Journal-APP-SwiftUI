d47e62ccb8f264c9bd27c1860fe67f91a0500f20
Navi9519 <dedicivanovic@gmail.com>
2024-09-25 09:27:07 +0200

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Ivan Dedic on 2024-09-25.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
